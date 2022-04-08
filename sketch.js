2be5cfc79eca8f8320e3cf92293ac4ca275c4c46
DummyCoder1 <100186733+DummyCoder1@users.noreply.github.com>
2022-04-08 09:06:57 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+var fruits = ["Apple", "Mango", "Banana", "Orange"];
+
+function setup() {
+    for(var i =0; i<fruits.length; i++ ){
+     console.log(fruits[i])
+    }
+
+    
+    
+
+}
+
+function draw() {
+    
+}
